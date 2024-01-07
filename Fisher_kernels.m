bf9a44ee9da63840ac1ea88e4107163197fb78cb
NickTy-byte <vasilakisnikos49@gmail.com>
2024-01-07 04:12:51 +0200

    Changes as per 04:12 07/01/2024

--- Fisher_kernels.m
+++ Fisher_kernels.m
@@ -1,21 +1,27 @@
 clear;clc;close all
 
-%%
+%% Load the YOLOv4 Object Detector
 yolo = yolov4ObjectDetector("csp-darknet53-coco");
-%%
+
+%% 
 filelocation = uigetdir;
 imds = imageDatastore(filelocation);
-%%
-i=0;reset(imds)
+
+%% Perform Object Detection
+
+i=0;
+reset(imds)
+
 scores= zeros(length(imds.Files),1);
 labelsYolo = zeros(length(imds.Files),1);
 
 totalFiles = length(imds.Files);
 loadingIcons = ['-', '\', '|', '/']; % Loading icon sequence
 
-tic
 % Create a waitbar
 h = waitbar(0, 'Processing...'); warning("off")
+
+tic
 
 while hasdata(imds)
     i=i+1;
@@ -55,8 +61,20 @@ close(h)
 Labeling_time = toc
 
 %%
+% Put the Labels to the corresponding structular place
+imds.Labels = Labels;
+
 % Find the Labels with Review 
 ind = find(Labels == "Review");
 
-% Find the images that correspond to those indexes
-Review_images = imds.Files(ind);
+% Find the images that correspond to those indexes and create a new image 
+% datastore that doesn't include them and also remove the labels from the 
+% labels array
+
+delimg = false(size(imds.Files)); % Initialize delimg array
+
+for i = 1:length(ind)
+    delimg(matches(imds.Files, imds.Files{ind(i),1})) = true;
+end
+new_imds = subset(imds, ~delimg);
+
