a2cbf5ce792a0a33080b63c1aef78f421334a1f2
NickTy-byte <vasilakisnikos49@gmail.com>
2024-01-12 03:21:46 +0200

    Changes as per 03:21 at 12/01/2024

--- Fisher_kernels.m
+++ Fisher_kernels.m
@@ -8,6 +8,7 @@ filelocation = uigetdir;
 imds = imageDatastore(filelocation);
 
 %% Assign Labels
+tic
 new_imds = assignLabels(imds,yolo);
 clear yolo
 %% Feature Extraction
@@ -23,7 +24,50 @@ FeatureMatrix.SIFT_Features_Matrix = vertcat(features(:).SIFT_Features);
 FeatureMatrix.RGB_Features_Matrix = vertcat(features(:).RGBfeatures);
 FeatureMatrix.Reduced_SIFT_Features_Matrix = vertcat(features(:).reduced_SIFT_features);
 FeatureMatrix.Reduced_RGB_Features_Matrix = vertcat(features(:).reduced_RGB_features);
+preprocessing_time = toc
 
 %% Gaussian Mixture Model
 
+k = 1:128;
+nK = numel(k);
 
+Sigma = 'diagonal';
+nSigma = numel(Sigma);
+
+SharedCovariance = {true,false};
+SCtext = {'true','false'};
+nSC = numel(SharedCovariance);
+
+RegularizationValue = 0.01;
+options = statset('MaxIter',1000,'Display','iter');
+X = FeatureMatrix.Reduced_SIFT_Features_Matrix;
+
+% Fit all models in parallel
+for m = 1:nSC
+    for i = 1:nK
+        gm{i,m} = fitgmdist(X,k(i),...
+                            'CovarianceType',Sigma,...
+                            'SharedCovariance',SharedCovariance{m},...
+                            'RegularizationValue',RegularizationValue,...
+                            'Options',options);
+        aic(i,m) = gm{i,m}.AIC;
+        bic(i,m) = gm{i,m}.BIC;
+        converged(i,m) = gm{i,m}.Converged;
+    end
+end
+
+allConverge = (sum(converged(:)) == nK*nSigma*nSC);
+
+figure
+bar(reshape(aic,nK,nSigma*nSC))
+title('AIC For Various $k$','Interpreter','latex')
+xlabel('$k$','Interpreter','Latex')
+ylabel('AIC')
+legend({'Diagonal-shared','Diagonal-unshared'})
+
+figure
+bar(reshape(bic,nK,nSigma*nSC))
+title('BIC For Various $k$','Interpreter','latex')
+xlabel('$c$','Interpreter','Latex')
+ylabel('BIC')
+legend({'Diagonal-shared','Diagonal-unshared'})   
